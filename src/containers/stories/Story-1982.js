71908cd24339a8fa270c82ef9efe064d7bdf4d82
Ernest annor <we2annor@gmail.com>
2018-10-01 10:56:32 +0100

    first commit

---
+++ src/containers/stories/Story-1982.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { Motion, spring } from 'react-motion'
+import '../../styles/containers/stories/story-1982.css'
+
+class S1982 extends Component {
+            next(){
+
+            }
+
+    render(){
+
+        return(
+            <div id="story" className="story story-1982">
+                <Motion defaultStyle={{opacity:0, scale: 0.5}} style={{opacity: spring(1), scale: spring(1)}}>
+                    {value=>
+                        <div style={{opacity: value.opacity, transform:`scale(${value.scale})`}}>
+                            <div className="content layer" data-depth="0.30">
+                                <div className="heading">
+                                    <h1 className="year">1982</h1>
+                                    <div className="label">My Story</div>
+                                    <div className="description">Born in Kumasi, Ghana, my passion for creativity and design was crafted from my love for drawing. I loved to draw as a child.</div>
+                                    <div className="btn btn-outline next" onClick={this.next.bind(this)}>NEXT</div>
+                                </div>
+                            </div>
+                        </div>
+                    }
+                </Motion>
+            </div>
+        )
+    }
+}
+
+export default S1982
---
+++ src/lib/app/actions.js
@@ -0,0 +1,11 @@
+export const showShadow = () => {
+    return {
+        type: 'SHOW_SHADOW'
+    }
+}
+
+export const hideShadow = () => {
+    return {
+        type: 'HIDE_SHADOW'
+    }
+}
